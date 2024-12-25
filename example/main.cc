450ef6a4999052688e930449f9293ecf60e884a4
suryanshvermaa <154125921+suryanshvermaa@users.noreply.github.com>
2024-12-25 22:41:39 +0530

    first hello world library that prints hello world

---
+++ example/main.cc
@@ -0,0 +1,7 @@
+#include<iostream>
+#include"cmake/hello.h"
+using namespace std;
+int main(){
+    printHello();
+    return 0;
+}
---
+++ src/hello.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "cmake/hello.h"
+
+void printHello(){
+    std::cout<<"Hello world"<<std::endl;
+}
